3808c5bf74dd77bc59b8daf66605041579cfa8f6
Andrey Ivanov <adin.ad@mail.ru>
2016-11-14 15:47:40 +0300

    Arbitrary width of DArray blocks

--- rgrid/rgcut.h
+++ rgrid/rgcut.h
@@ -8,21 +8,25 @@
 
 #include "rgrid/types.h"
 
+#include <iostream>
+#include <vector>
+#include <map>
+
 namespace rgrid {
-	
+
 /**
  * \brief Class represents partitioning of rectangular structure (DArray)
- * 
+ *
  * It automatically divides 3d rectangular area into specified number of parts
  * with number of nodes that differ not more than one in each direction.
- * 
+ *
  * \tparam I type of indexes (i.e. int, long)
  */
 template <typename I>
 class RGCut {
 public:
 	/**
-	 * Use setCutParams() if RGCut created by this constructor 
+	 * Use setCutParams() if RGCut created by this constructor
 	 */
 	RGCut() {
 		setCutParams(1, 1, 1, 1, 1, 1);
@@ -45,19 +49,6 @@ public:
 		setCutParams(Dim3D<I>(sz), Dim3D<I>(pt));
 	}
 	/**
-	 * \param[in] sz is entire size in nodes in each direction
-	 * \param[in] pt is number of parts to devide
-	 */
-	void setCutParams(const Dim3D<I> sz, const Dim3D<I> pt) {
-		for (CartDir d = X; d != ALL_DIRS; d = static_cast<CartDir>(d+1)) {
-			parts[d] = pt[d];
-			size[d] = sz[d];
-			ml[d] = sz[d] / pt[d];
-			iml[d] = sz[d] % pt[d];
-		}
-		allParts = pt[X] * pt[Y] * pt[Z];
-	}
-	/**
 	 * \brief Set partitioning params
 	 * \param[in] szX,szY,szZ is entire size in each direction
 	 * \param[in] ptX,ptY,ptZ is number of parts to devide
@@ -66,6 +57,40 @@ public:
 		I sz[ALL_DIRS] = {szX, szY, szZ};
 		I pt[ALL_DIRS] = {ptX, ptY, ptZ};
 		setCutParams(sz, pt);
+	}
+	/**
+	 * \param[in] sz is entire size in nodes in each direction
+	 * \param[in] pt is number of parts to devide
+	 */
+	void setCutParams(const Dim3D<I> sz, const Dim3D<I> pt) {
+		Dim3D<std::vector<I> > width;
+		for (CartDir d = X; d != ALL_DIRS; ++d) {
+			for (I i = 0; i != pt[d]; ++i) {
+				I w = sz[d] / pt[d] + (i < sz[d] % pt[d] ? 1 : 0);
+				if (w > 0) width[d].push_back(w);
+			}
+		}
+		setCutParams(width);
+	}
+	/**
+	 * \param[in] width is width of each part
+	 */
+	void setCutParams(const Dim3D<std::vector<I> >& width) {
+		this->width = width;
+		for (CartDir d = X; d != ALL_DIRS; ++d) {
+			locator[d].clear();
+			RG_ASSERT(width[d].size() > 0, "Empty dimension in setCutParams");
+			parts[d] = width[d].size();
+			start[d].resize(width[d].size());
+			start[d].at(0) = 0;
+			locator[d][width[d].at(0)] = 0;
+			for (typename std::vector<I>::size_type i = 1; i < width[d].size(); ++i) {
+				start[d].at(i) = start[d].at(i-1) + width[d].at(i-1);
+				locator[d][start[d].at(i) + width[d].at(i)] = i;
+			}
+			size[d] = start[d].at(width[d].size()-1) + width[d].at(width[d].size()-1);
+		}
+		allParts = parts[X] * parts[Y] * parts[Z];
 	}
 	/**
 	 * \brief number of all parts
@@ -79,7 +104,7 @@ public:
 	 * \brief number of parts in each direction
 	 */
 	Dim3D<I> numParts3() const { return parts; }
-	/** 
+	/**
 	 * \brief Get index of part in linear array
 	 */
 	I linInd(const I i, const I j, const I k) const {
@@ -94,107 +119,67 @@ public:
 		vec[Y] = lin2 / parts[X];
 		vec[X] = lin2 % parts[X];
 	}
-	/** 
+	/**
 	 * \brief Convert linear index to i, j or k
 	 */
-	void vecInd(const I lin, CartDir dir) const {
+	I vecInd(const I lin, CartDir dir) const {
 		I vec[ALL_DIRS];
 		vecInd(lin, vec);
-		return vec[dir]; 
+		return vec[dir];
 	}
 	/**
 	 * \brief Number of all nodes
 	 */
 	I numNodes() const { return size[X] * size[Y] * size[Z]; }
-	/** 
+	/**
 	 * \brief Number of nodes in all parts in each direction
 	 */
 	I numNodes(CartDir dir) const { return size[dir]; }
-	/** 
+	/**
 	 * \brief Number of nodes in all parts in each direction
 	 */
 	Dim3D<I> numNodes3() const { return size; }
-	/** 
-	 * \brief Locate index of node in its part
+	/**
+	 * \brief Get local index from global
 	 * \param[in] dir direction to search
-	 * \param[in] contIdx index of node
+	 * \param[in] contIdx global index of node
 	 */
 	I locateIndex(const CartDir dir, const I contIdx) const {
-		if (contIdx < iml[dir] * (ml[dir] + 1)) { 
-			return contIdx % (ml[dir] + 1);
-		} else {
-			return ml[dir] - 1 - ((size[dir] - 1 - contIdx) % ml[dir]);
-		}
+		I partNum = locatePart(dir, contIdx);
+		return contIdx - partOrigin(dir, partNum);
 	}
-	/** 
+	/**
 	 * \brief Locate index of part in specified direction in which specified node contained
 	 * \param[in] dir direction of parts
-	 * \param[in] contIdx index of node in container
+	 * \param[in] contIdx global index of node
 	 */
 	I locatePart(const CartDir dir, const I contIdx) const {
-		if (contIdx < iml[dir] * (ml[dir] + 1)) { 
-			return contIdx / (ml[dir] + 1);
-		} else {
-			return parts[dir] - 1 - ((size[dir] - 1 - contIdx) / ml[dir]);
-		}
+		return locator[dir].upper_bound(contIdx)->second;
 	}
-	/** 
+	/**
 	 * \brief Get origin of part
 	 */
 	I partOrigin(const CartDir dir, const I partNum) const {
-		return partNum <= iml[dir] ? (ml[dir] + 1) * partNum : size[dir] - ml[dir] * (parts[dir] - partNum);
+		return start[dir].at(partNum);
 	}
 	/**
 	 * \brief Get origin of part
 	 */
 	Dim3D<I> partOrigin(const Dim3D<I>& partNum) const {
-		Dim3D<I> r;
-		for (CartDir d = X; d < ALL_DIRS; ++d) {
-			r[d] = partNum[d] <= iml[d] ? (ml[d] + 1) * partNum[d] : size[d] - ml[d] * (parts[d] - partNum[d]);
-		}
-		return r;
+		return Dim3D<I>(start[X].at(partNum[X]), start[Y].at(partNum[Y]), start[Z].at(partNum[Z]));
 	}
-	/** 
+	/**
 	 * \brief Get part nodes number
 	 */
 	I partNodes(const CartDir dir, const I partNum) const {
-		return partNum < iml[dir] ? ml[dir] + 1 : ml[dir];
+		return width[dir].at(partNum);
 	}
-	/** 
+	/**
 	 * \brief Get part nodes number
 	 * \param partPos coordinate of part
 	 */
 	Dim3D<I> partNodes(const Dim3D<I>& partPos) const {
-		Dim3D<I> r;
-		for (CartDir d = X; d < ALL_DIRS; ++d) {
-			r[d] = partPos[d] < iml[d] ? ml[d] + 1 : ml[d];
-		}
-		return r;
-	}
-	/**
-	 * \brief Make the same RGcut as specified
-	 */
-	RGCut(const RGCut<I>& rhs) {
-		for (CartDir d = X; d != ALL_DIRS; d = static_cast<CartDir>(d+1)) {
-			parts[d] = rhs.parts[d];
-			size[d] = rhs.size[d];
-			ml[d] = rhs.ml[d];
-			iml[d] = rhs.iml[d];
-		}
-		allParts = rhs.allParts;
-	}
-	/**
-	 * \brief Make the same RGcut as specified
-	 */
-	RGCut<I>& operator=(const RGCut<I>& rhs) {
-		for (CartDir d = X; d != ALL_DIRS; d = static_cast<CartDir>(d+1)) {
-			parts[d] = rhs.parts[d];
-			size[d] = rhs.size[d];
-			ml[d] = rhs.ml[d];
-			iml[d] = rhs.iml[d];
-		}
-		allParts = rhs.allParts;
-		return *this;
+		return Dim3D<I>(width[X].at(partPos[X]), width[Y].at(partPos[Y]), width[Z].at(partPos[Z]));
 	}
 private:
 	/* number of parts in each direction */
@@ -203,10 +188,12 @@ private:
 	I allParts;
 	/* number of nodes in each direction in ALL parts */
 	Dim3D<I> size;
-	/* minimal length - length in nodes of last parts in each direction */
-	Dim3D<I> ml;
-	/* index of part before that number of nodes equals ml[dir] + 1 */
-	Dim3D<I> iml;
+	/* origins of blocks */
+	Dim3D<std::vector<I> > start;
+	/* width of blocks */
+	Dim3D<std::vector<I> > width;
+	/* key - first index of next block, val - current block num */
+	Dim3D<std::map<I, I> > locator;
 };
 
 } /* namespace rgrid */
